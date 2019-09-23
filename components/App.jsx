be47116f0cad8f4cc3bf4d468a6520b67e9ac938
nigel-daniels <nigel.daniels@me.com>
2019-09-23 14:06:26 -0700

    Improved css loading

--- components/App.jsx
+++ components/App.jsx
@@ -1,4 +1,4 @@
-import '../css/display.css';
+import '../css/index.css';
 
 import React from 'react';
 
--- server.js
+++ server.js
@@ -29,7 +29,6 @@ function htmlTemplate( reactDom ) {
             <meta charset="utf-8">
 			<meta viewport="width=device-width, initial-scale=1, shrink-to-fit=no">
             <title>Display</title>
-			<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"/>
 			${initialStyleTag}
 		</head>
 
