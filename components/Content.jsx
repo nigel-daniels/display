88ac04472a513b31fc2c1c6b18de085b87a52687
nigel-daniels <nigel.daniels@me.com>
2019-09-26 16:58:07 -0700

    Simplified display.

--- components/Content.jsx
+++ components/Content.jsx
@@ -1,5 +1,5 @@
 import fs from 'fs';
-import path from 'path';
+//import path from 'path';
 import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Table from 'react-bootstrap/Table';
@@ -13,15 +13,14 @@ export default class Content extends React.Component {
 		let files = fs.readdirSync(file_path);
 
 		content = files.map(file => {
-			let stats = fs.lstatSync(file_path + '/' + file);
+			//let stats = fs.lstatSync(file_path + '/' + file);
 
 			return (<tr key='{{file}}'>
 				<td>{file}</td>
-				<td>{path.extname(file)}</td>
-				<td>{stats.size}</td>
 			</tr>);
 		});
-
+		//<td>{path.extname(file)}</td>
+		//<td>{stats.size}</td>
 		return <tbody>
 			{content}
 		</tbody>;
@@ -34,14 +33,12 @@ export default class Content extends React.Component {
 				<Table className="file_list" responsive striped bordered hover>
 					<thead>
 						<th>Filename</th>
-						<th>Type</th>
-						<th>Size</th>
 					</thead>
 					{this.tableContent()}
 				</Table>
-				<p className="lead">Pin a footer to the bottom of the viewport in desktop browsers with this custom HTML and CSS. A fixed navbar has been added with <code>padding-top: 60px;</code> on the <code>main &gt; .container</code>.</p>
-				<p>Back to <a href="/docs/4.3/examples/sticky-footer/">the default sticky footer</a> minus the navbar.</p>
 			</Container>
 		);
 	}
+	//<th>Type</th>
+	//<th>Size</th>
 }
