85c22e4dd0f67968df39804a75930ec672dfb4f3
Nigel Daniels <nigel.daniels@initiatethinking.com>
2019-10-06 22:30:40 -0700

    Using event

--- components/ImageCard.jsx
+++ components/ImageCard.jsx
@@ -1,49 +1,23 @@
 import React from 'react';
-import { Card, CardImg, CardBody, CardTitle, Button,
-	Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
+import { Card, CardImg, CardBody, CardTitle, Button, Row} from 'reactstrap';
 
 export default class ImageCard extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {show: false};
 
-		this.toggle = this.toggle.bind(this);
-	}
 
-	handleClose() {
-		this.setState({show: false});
-	}
+	delete(event) {
+		console.log('delete called ' + JSON.stringify(event));
 
-	toggle() {
-		console.log('show called ' + this.state.show);
-		this.setState(prevState => ({show: !prevState.show}));
 	}
 
 	render() {
 
-		return (<div>
-			<Card className="image-card">
-				<CardImg top src={this.props.file} />
-				<CardBody>
-					<CardTitle>{this.props.file}</CardTitle>
-					<Button outline color="danger" onClick={this.toggle}>Delete</Button>
-				</CardBody>
-			</Card>
-			<Modal isOpen={this.state.show} toggle={this.toggle}>
-				<ModalHeader toggle={this.toggle}>
-					Delete
-				</ModalHeader>
-				<ModalBody>You are about to delete {this.props.file}</ModalBody>
-				<ModalFooter>
-					<Button variant="secondary" onClick={this.toggle}>
-						Cancel
-					</Button>
-					<Button variant="primary" onClick={this.toggle}>
-						Delete
-					</Button>
-				</ModalFooter>
-			</Modal>
-		</div>
-		);
+		return (<Card className="image-card">
+			<CardImg top src={this.props.file} />
+			<CardBody>
+				<CardTitle>{this.props.file}</CardTitle>
+				<Row><Button outline color="danger" onClick={() => this.delete}>Delete</Button><div id={this.props.file}></div></Row>
+			</CardBody>
+		</Card>);
+
 	}
 }
