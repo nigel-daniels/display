39a671b06b8567177d4f4ab4735fb32f8f472fd9
Nigel Daniels <nigel.daniels@initiatethinking.com>
2019-11-26 18:21:27 -0800

    Switched to using fim.

--- server/services/display_services.js
+++ server/services/display_services.js
@@ -62,7 +62,7 @@ export function deleteFile(req, res) {
 
 export function  startDisplay(req, res) {
 	debug('startDisplay, called');
-	process.exec('fbi -a -t 5 -1 --noverbose ./display/dist/public/images-display/', (err, stdout, stderr) => {
+	process.exec('fim -a -q -C "while(1){next;display;sleep \'20\';};" /home/pi/display/dist/public/images-display/', (err, stdout, stderr) => {
 		if (err) {
 			return res.status(500).send({message: 'There was an error starting the display.'});
 		} else {
@@ -74,7 +74,7 @@ export function  startDisplay(req, res) {
 
 export function  stopDisplay(req, res) {
 	debug('stopDisplay, called');
-	process.exec('pkill -f "fbi"', (err, stdout, stderr) => {
+	process.exec('pkill -f "fim"', (err, stdout, stderr) => {
 		if (err) {
 			return res.status(500).send({message: 'There was an error starting the display.'});
 		} else {
